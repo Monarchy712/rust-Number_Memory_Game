0dcad8b2b7b2e33e9043acfca57b5fc21dd60cd5
Samyaak <samyaakjain712@gmail.com>
2026-04-02 12:13:18 +0530

    add game logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rand::Rng;
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Memory Game!");
+    println!("Try to remember the sequence of numbers.");
+    let mut sequence: Vec<u32> = Vec::new();
+    let mut round = 1;
+    loop{
+        println!("Round {}", round);
+
+        let new_number = rand::thread_rng().gen_range(1..=9);
+        sequence.push(new_number);
+
+        println!("Memorize:");
+        for num in &sequence{
+            print!("{} ", num);
+        }
+        println!();
+
+        thread::sleep(time::Duration::from_secs(2));
+
+    }
 }
